5fd30733cb4ba5d87657fd4d9b45f1d6d54a6ded
OmkarG <omkargample@gmail.com>
2025-01-23 09:37:08 +0530

    Added new code and files

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
+require('dotenv').config();
 const express = require("express");
+let sequelize = require("./lib/sequelize");
+let user = require("./models/user");
 const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("static"));
-let port = 3000;
+let port = process.env.PORT || 3000;
 
-let users = [{id:1,username:"omk23",name:"omkar",repoCount:50,location:"London"},
-    {id:2,username:"aka14",name:"akash",repoCount:50,location:"usa"}];
+/*let users = [{id:1,username:"omk23",name:"omkar",repoCount:50,location:"London"},
+    {id:2,username:"aka14",name:"akash",repoCount:50,location:"usa"}];*/
 
-app.get('/users', (req,res)=>{
-    res.status(200).json(users);
+sequelize.sync().then(()=>{
+    console.log("Database connected and sync")
+}).catch((error)=>{
+    console.log("unable to connect to database", error);
 });
 
-app.get("/users/id/:id", (req,res)=>{
+app.get('/users', async (req,res)=>{
+    try{
+        const result = await user.findAll();
+        res.status(200).json({users:result});
+    }catch(error){
+        res.status(500).json({error:error.message});
+    }
+});
+
+app.get("/users/id/:id", async (req,res)=>{
     const id = parseInt(req.params.id);
-    const result = users.find(user => user.id === id);
-    res.status(200).json(result);
+    try{
+        
+        const result = await user.findByPk(id);
+        if(result){
+            res.status(200).json(result);
+        }
+        else{
+            res.status(404).json({message:"No Users found for id"});
+        }
+        
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error:"failed to fetch user"});
+    }
 });
 
 app.listen(port, ()=>{  
---
+++ models/user.js
@@ -0,0 +1,33 @@
+// models/user.js
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../lib/sequelize');
+
+const user = sequelize.define(
+    'user',
+    {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        repoCount: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        location: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        tableName: 'users',
+        timestamps: true,
+    }
+);
+
+module.exports = user;
